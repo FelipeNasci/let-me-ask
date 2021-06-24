bc00a18a40f7a2b1e590a1d0e9f1923bd0eaf466
Diego Nascimento <dfnascimento@live.com>
2021-06-24 18:51:28 -0300

    feat: add styles HomePage

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import illustrationImg from "../../assets/images/illustration.svg";
 import logoImg from "../../assets/images/logo.svg";
 import googleIconImg from "../../assets/images/google-icon.svg";
 import '../../styles/auth.scss'
+import { Button } from "../../components/button";
 
 export const Home = () => {
   return (
@@ -14,14 +15,14 @@ export const Home = () => {
       <main className="main-content">
         <div>
           <img src={logoImg} alt="letmeask" />
-          <button>
+          <button className='create-room'>
             <img src={googleIconImg} alt="logo do Google" />
             Crie sua sala com o Google
           </button>
-          <div>ou entre em uma sala</div>
-          <form >
-            <input type="text" placeholder="Digite o código da sala"/>
-            <button type="submit">Entrar na sala</button>
+          <div className="separator">ou entre em uma sala</div>
+          <form>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
       </main>
